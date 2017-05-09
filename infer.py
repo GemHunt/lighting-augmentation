4a7e688cd2b017f4c217a2e865037c35f656e427
Paul Krush <pkrush@gemhunt.com>
2017-05-09 12:44:55 -0500

    Clean up & Create test image set

--- infer.py
+++ infer.py
@@ -1,15 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
 import cv2
-import glob
-import random
 import os
 
-# Make sure that caffe is on the python path:
-# sys.path.append('~/caffe/python') using the ~ does not work, for some reason???
 sys.path.append('/home/pkrush/caffe/python')
 import caffe
+
 
 def get_classifier(model_name, crop_size):
     model_dir = model_name + '/'
@@ -34,6 +30,7 @@ def get_labels(model_name):
     labels = [line.rstrip('\n') for line in open(labels_file)]
     return labels;
 
+
 def get_caffe_image(crop, crop_size):
     # this is how you get the image from file:
     # coinImage = [caffe.io.load_image("some file", color=False)]
@@ -44,12 +41,29 @@ def get_caffe_image(crop, crop_size):
     # Caffe wants a list so []:
     return [caffe_image];
 
+
+def get_composite_image(images, rows, cols):
+    crop_rows, crop_cols, channels = images[0].shape
+    composite_rows = crop_rows * rows
+    composite_cols = crop_cols * cols
+    composite_image = np.zeros((composite_rows, composite_cols, 3), np.uint8)
+    key = 0
+    for x in range(0, rows):
+        for y in range(0, cols):
+            key += 1
+            if len(images) <= key:
+                break
+            if images[key] is not None:
+                composite_image[x * crop_rows:((x + 1) * crop_rows), y * crop_cols:((y + 1) * crop_cols)] = images[key]
+    return composite_image
+
+
 crop_size = 28
 heads_tails = get_classifier("heads_tails", crop_size)
 heads_tails_labels = get_labels('heads_tails')
 count = 0
 
-#design_crop_dir = '/home/pkrush/data/cents-test/5/'
+# design_crop_dir = '/home/pkrush/data/cents-test/5/'
 design_crop_dir = '/home/pkrush/cent-designs3'
 
 image_ids = []
@@ -57,18 +71,20 @@ image_ids = []
 for root, dirnames, walk_filenames in os.walk(design_crop_dir):
     for filename in walk_filenames:
         if filename.endswith('.png'):
-            image_id = int(filename.replace('.png', ''))/100
+            image_id = int(filename.replace('.png', '')) / 100
             image_ids.append([image_id, root + '/' + filename])
 
 image_ids = sorted(image_ids)
-
 coin_scores = {}
+thumbnails = {}
 
-for image_id,filename in image_ids:
+for image_id, filename in image_ids:
     coin_id = image_id / 100
     crop = cv2.imread(filename)
     if crop is None:
         continue
+    if image_id % 100 == 54:
+        thumbnails[coin_id] = crop.copy()
     crop = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
     score = heads_tails.predict(get_caffe_image(crop, crop_size), oversample=False)
     if coin_id in coin_scores.iterkeys():
@@ -77,7 +93,34 @@ for image_id,filename in image_ids:
         coin_scores[coin_id] = score
     coin_type = heads_tails_labels[np.argmax(score)]
     max_value = np.amax(score)
-    print image_id, coin_type, score, max_value
+    # print image_id, coin_type, score, max_value
 
-print coin_scores
+results = []
+
+for coin_id, score in coin_scores.iteritems():
+    coin_type = heads_tails_labels[np.argmax(score)]
+    # There are 57 images and 100/57 is needed to take it to 100%
+    max_value = np.amax(score) * 100 / 57
+    results.append([coin_id, coin_type, max_value])
+
+results = sorted(results, key=lambda result: result[2], reverse=True)
+results = sorted(results, key=lambda result: result[1], reverse=True)
+images = []
+
+for coin_id, coin_type, max_value in results:
+    crop = thumbnails[coin_id]
+    #font = cv2.FONT_HERSHEY_SIMPLEX
+    #cv2.putText(crop, str(max_value)[0:5], (4, 20), font, .7, (0, 255, 0), 2)
+    #cv2.putText(crop, str(coin_type), (4, 90), font, .7, (0, 255, 0), 2)
+    images.append(crop)
+
+composite = get_composite_image(images, 10, 10)
+cv2.namedWindow("results")
+
+while True:
+    cv2.imshow("results",composite)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+
 cv2.destroyAllWindows()
--- label_type.py
+++ label_type.py
@@ -7,44 +7,43 @@ import shutil
 import glob
 import random
 
-data_dir = '/home/pkrush/cent-date-models/metadata/'
-crop_dir = '/home/pkrush/cent-dates/'
-classify_dir = '/home/pkrush/cent-dates-classify/'
+def get_rotated_image(image,before_rotate_size,angle, pixels_to_jitter):
+    center_x = before_rotate_size / 2 + (random.random() * pixels_to_jitter * 2) - pixels_to_jitter
+    center_y = before_rotate_size / 2 + (random.random() * pixels_to_jitter * 2) - pixels_to_jitter
+    rot_image = image.copy()
+    m = cv2.getRotationMatrix2D((center_x, center_y), angle, 1)
+    cv2.warpAffine(rot_image, m, (before_rotate_size, before_rotate_size), rot_image, cv2.INTER_CUBIC)
+    # This is hard coded for 28x28.
+    rot_image = cv2.resize(rot_image, (41, 41), interpolation=cv2.INTER_AREA)
+    rot_image = rot_image[6:34, 6:34]
+    return rot_image
 
-def get_filename(coin_id):
-    dir = crop_dir + str(coin_id / 100) + '/'
-    return dir + str(coin_id).zfill(5) + str(54).zfill(2) + '.png'
+def create_data_set(crop_dir,new_crop_dir,before_rotate_size,total_coins,crop):
+    image_ids = []
+    coins_ids = set()
 
-def copy_images_for_classification():
-    ground_truth_dates = pickle.load(open(data_dir + 'ground_truth_dates.pickle', "rb"))
-    ground_truth_dates = sorted(ground_truth_dates, key=lambda x: x[3], reverse=False)
-    if not os.path.exists(classify_dir):
-        os.mkdir(classify_dir)
+    for root, dirnames, walk_filenames in os.walk(crop_dir):
+        for filename in walk_filenames:
+            if filename.endswith('.png'):
+                image_id = int(filename.replace('.png', ''))
+                image_ids.append([image_id, root , filename])
+    print 'Total images in ' + crop_dir + ': ' + len(image_ids)
 
-    for seed_id, coin_id, result, labeled_date, bad_angle, bad_image in ground_truth_dates:
-        if labeled_date < 1900:
-            continue
-        dir = crop_dir + str(coin_id / 100) + '/'
-        new_dir = classify_dir + str(labeled_date) + '/'
-        if not os.path.exists(new_dir):
-            os.mkdir(new_dir)
-        for image_id in range(0,57):
-            filename = str(coin_id).zfill(5) + str(image_id).zfill(2) + '.png'
-            old_filename = dir + filename
-            new_filename = new_dir + filename
-            shutil.copyfile(old_filename,new_filename)
+    image_ids = sorted(image_ids)
 
-
-def save_and_exit(labeled_dates,ground_truth_dates):
-    #pickle.dump(labeled_dates, open(data_dir + 'labeled_dates.pickle', "wb"))
-    new_ground_truth_dates = []
-    for seed_id, coin_id, result, labeled_date, bad_angle, bad_image in ground_truth_dates:
-       if coin_id in labeled_dates.iterkeys():
-           labeled_date = labeled_dates[coin_id]
-       new_ground_truth_dates.append([seed_id, coin_id, result, labeled_date, bad_angle, bad_image])
-
-    pickle.dump(new_ground_truth_dates, open(data_dir + 'ground_truth_dates.pickle', "wb"))
-    sys.exit()
+    for image_id, root, filename in image_ids:
+        coin_id = image_id / 100
+        coins_ids.add(coin_id)
+        if len(coins_ids) <= total_coins:
+            image = cv2.imread(root + '/' + filename)
+            if image == None:
+                print 'Is None: ' + filename
+            else:
+                if crop:
+                    image = get_rotated_image(image, before_rotate_size, angle = 0, pixels_to_jitter = 0)
+                else:
+                    image = cv2.resize(image, (before_rotate_size, before_rotate_size), interpolation=cv2.INTER_AREA)
+                cv2.imwrite(new_crop_dir + filename,image)
 
 
 def create_design_data_set(labeled_designs,design_crop_dir,image_dir,test):
@@ -72,109 +71,10 @@ def create_design_data_set(labeled_designs,design_crop_dir,image_dir,test):
             image = cv2.resize(image, (before_rotate_size, before_rotate_size), interpolation=cv2.INTER_AREA)
             for count in range(0,angles):
                 angle = random.random() * 360
-                center_x = before_rotate_size / 2 + (random.random() * pixels_to_jitter * 2) - pixels_to_jitter
-                center_y = before_rotate_size / 2 + (random.random() * pixels_to_jitter * 2) - pixels_to_jitter
-                rot_image = image.copy()
-                m = cv2.getRotationMatrix2D((center_x, center_y), angle, 1)
-                cv2.warpAffine(rot_image, m, (before_rotate_size, before_rotate_size), rot_image, cv2.INTER_CUBIC)
-                # This is hard coded for 28x28.
-                rot_image = cv2.resize(rot_image, (41, 41), interpolation=cv2.INTER_AREA)
-                rot_image = rot_image[6:34, 6:34]
+                rot_image = get_rotated_image(image, before_rotate_size, angle, pixels_to_jitter)
                 rotated_filename = filename.replace('.png', str(count).zfill(2) + '.png')
                 cv2.imwrite(class_dir + rotated_filename,rot_image)
     sys.exit()
-
-def label_date():
-    cv2.namedWindow("next")
-
-    ground_truth_dates = pickle.load(open(data_dir + 'ground_truth_dates.pickle', "rb"))
-    #labeled_dates = pickle.load(open(data_dir + 'labeled_dates.pickle', "rb"))
-    labeled_dates = {}
-    ground_truth_date_dict = {}
-
-    images = {}
-
-    ground_truth_dates = sorted(ground_truth_dates, key=lambda x: x[3], reverse=False)
-
-    for count in range(0,len(ground_truth_dates)):
-        seed_id,coin_id, result,labeled_date,bad_angle,bad_image = ground_truth_dates[count]
-        if coin_id in labeled_dates.iterkeys():
-            pass
-            #continue
-        filename = get_filename(coin_id)
-        if not os.path.exists(filename):
-            labeled_dates[coin_id] = -2
-            continue
-        image = cv2.imread(filename)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(image, str(labeled_date % 100)[0:5], (4, 20), font, .7, (0, 255, 0), 2)
-
-        images[coin_id] = image
-
-        last_labeled_date = 0
-
-    for count in range(0, len(ground_truth_dates)):
-        seed_id, coin_id, result, old_labeled_date, bad_angle, bad_image = ground_truth_dates[count]
-        if not coin_id in images.iterkeys():
-            continue
-        next = np.zeros((56, 560, 3), np.uint8)
-        for count2 in range(0,10):
-            if count + count2 >= len(ground_truth_dates):
-                continue
-            next_coin_id = ground_truth_dates[count + count2 ][1]
-            if next_coin_id in images.iterkeys():
-                next_image = images[next_coin_id]
-            else:
-                next_image = np.zeros((56, 56, 3), np.uint8)
-            next[0:56,(9-count2) * 56:((10-count2) * 56)] = next_image
-
-        decade  = -1
-        year = -1
-        labeled_date = -999
-
-        while True:
-            # display the image and wait for a keypress
-            cv2.imshow("image", images[coin_id])
-            cv2.imshow("next", next)
-
-            key = cv2.waitKey(1) & 0xFF
-            if key !=255:
-                print key
-
-            if key == ord("a"):
-                labeled_date = 0
-            if key == ord("b"):
-                labeled_date = -1
-
-
-            if 47 < key < 58:
-                if decade == -1:
-                    decade = key - 48
-                else:
-                    year = key - 48
-                    if decade < 2:
-                        labeled_date = 2000 + decade * 10 + year
-                    else:
-                        labeled_date  = 1900 + decade * 10 + year
-
-            if key == 10: #enter
-                labeled_date = last_labeled_date
-
-            if labeled_date != -999:
-                 labeled_dates[coin_id] = labeled_date
-                 print labeled_date
-                 last_labeled_date = labeled_date
-                 break
-
-            if key == 27: #esc
-                break
-
-            if key == ord("q"):
-                save_and_exit(labeled_dates,ground_truth_dates)
-
-    save_and_exit(labeled_dates,ground_truth_dates)
-    cv2.destroyAllWindows()
-
 
 def label_heads_tails():
     cv2.namedWindow("next")
@@ -248,7 +148,13 @@ def label_heads_tails():
     create_design_data_set(labeled_designs,design_crop_dir,image_dir,test)
     cv2.destroyAllWindows()
 
+before_rotate_size = 56
+crop_dir = '/home/pkrush/data/cents-test/'
+new_crop_dir = '/home/pkrush/cents/'
+crop_crop_dir = '/home/pkrush/cents-cropped/'
 
 
-label_heads_tails()
-#copy_images_for_classification()
+#total_coins is 1002 because 2 are missing images:
+#create_data_set(crop_dir,new_crop_dir,before_rotate_size,total_coins=1002,crop=False)
+#create_data_set(new_crop_dir,crop_crop_dir,before_rotate_size,total_coins=1000,crop=True)
+#label_heads_tails()
